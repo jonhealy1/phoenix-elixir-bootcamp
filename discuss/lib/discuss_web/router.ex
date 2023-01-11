e411f63748b8908b4052f9b4eb9a658e98ee1023
jonhealy1 <jonathan.d.healy@gmail.com>
2023-01-11 15:18:18 +0300

    add routes

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -18,6 +18,9 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/topics/new", TopicController, :new
+    get "/topics", TopicController, :index
+    post "/topics", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
