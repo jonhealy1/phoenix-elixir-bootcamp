e84b69dae9f84572eda4a953f9647f82dd20baad
jonhealy1 <jonathan.d.healy@gmail.com>
2023-01-11 13:35:08 +0300

    add materialize, discussions link

--- discuss/config/dev.exs
+++ discuss/config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :discuss, Discuss.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "comp",
+  password: "",
   hostname: "localhost",
   database: "discuss_dev",
   stacktrace: true,
